570381ce711a3f0086a4363aef7763c4f5bc2ab9
Pradnya Sutar <pradnyasutar05@gmail.com>
2025-12-10 20:09:14 +0530

    area perimeter

---
+++ geometry.py
@@ -0,0 +1,6 @@
+def cal_rect_area(len,br):
+    print("area of rectangle:",len*br)
+
+
+def cal_rect_peri(len,br):
+    print("perimeter of rectangle:",2*(len+br))
--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import arithmetic
+import geometry
 
 print("Hello World!!")
 
@@ -8,3 +9,9 @@ num2=int(input("enter num2:"))
 arithmetic.add(num1,num2)
 
 arithmetic.substract(num1,num2)
+
+len=int(input("enter length:"))
+br=int(input("enter breadth:"))
+
+geometry.cal_rect_area(len,br)
+geometry.cal_rect_peri(len,br)
